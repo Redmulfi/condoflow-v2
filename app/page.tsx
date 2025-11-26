1a3d03abe7ca15851910ced4c7f899dae48c8e3f
Geovani <redmulfi@gmail.com>
2025-11-26 00:36:07 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>CondoFlow v2</h1>
+      <p>Multi-tenant property management system</p>
+      <p>Production-ready template</p>
+    </div>
+  )
+}
